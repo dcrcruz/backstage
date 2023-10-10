fb54b8bcc697e5124e35a9514aa40535db163f3c
Patrik Oldsberg <poldsberg@gmail.com>
2023-10-10 17:02:45 +0200

    initial DI legacy interop
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/app-next/src/App.tsx
+++ packages/app-next/src/App.tsx
@@ -28,19 +28,29 @@ import { entityRouteRef } from '@backstage/plugin-catalog-react';
 
 /*
 
-# Notes
+# Legacy interoperability
 
-TODO:
- - proper createApp
- - connect extensions and plugins, provide method?
- - higher level API for creating standard extensions + higher order framework API for creating those?
- - extension config schema + validation
- - figure out how to resolve configured extension ref to runtime value, e.g. '@backstage/plugin-graphiql#GraphiqlPage'
- - make sure all shorthands work + tests
- - figure out package structure / how to ship, frontend-plugin-api/frontend-app-api
- - figure out routing, useRouteRef in the new system
- - Legacy plugins / interop
- - dynamic updates, runtime API
+Use-cases (prioritized):
+ 1. Slowly migrate over an existing app to DI, piece by piece
+ 2. Use a legacy plugin in a new DI app
+ 3. Use DI in an existing legacy app
+
+Starting point: use-case #1
+
+Potential solutions:
+ 1. Codemods (we're not considering this for now)
+ 2. Legacy apps are migrated bottom-up, i.e. keep legacy root, replace pages with DI
+ 3. Legacy apps are migrated top-down i.e. switch out base to DI, legacy adapter allows for usage of existing app structure
+
+Chosen path: #3
+
+Existing tasks:
+  - Adopters can migrate their existing app gradually (~4)
+    - Example-app uses legacy base with DI adapters
+    - Create an API that lets you inject DI into existing apps - working assumption is that this is enough
+  - Adopters can use legacy plugins in DI through adapters (~8)
+    - App-next uses DI base with legacy adapters
+    - Create a legacy adapter that is able to take an existing extension tree
 
 */
 
